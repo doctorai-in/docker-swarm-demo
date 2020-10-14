f00ad0c7e9815c6a9812f95377278bdc47541b09
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-10-14 15:55:01 +0530

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -3,5 +3,7 @@ mkdir .ssh
 chmod 700 .ssh
 touch .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
-cp -f ~/docker-swarm-demo/authorized_keys .ssh/
+cp -f /home/ubuntu/docker-swarm-demo/authorized_keys .ssh/
 chmod 600 .ssh/authorized_keys
+git clone https://github.com/docker/example-voting-app.git
+cd example-voting-app
